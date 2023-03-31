c2f2edfeea05a19edd4c11d1bf8a13b6dcdf4663
Faizal Amiruddin <f.a.faizal.amiruddin@gmail.com>
2023-03-31 09:43:15 +0700

    feat(home): init slider

---
+++ components/SliderItem.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { StarIcon } from '@heroicons/vue/20/solid'
+
+export interface Props {
+  movieId: number;
+  title: string;
+  year: string;
+  genre: string;
+  rating: number;
+  image: string;
+  overview: string;
+  active: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  movieId: 0,
+  title: '',
+  year: '',
+  genre: '',
+  rating: 0,
+  image: '',
+  overview: '',
+  active: false,
+})
+
+const imgSrc = ref(props.image ? `https://image.tmdb.org/t/p/w500${props.image}` : '/img/image-not-found.png')
+</script>
+
+<template>
+  <div
+    :class="{
+      'py-5 pl-5': true,
+      'opacity-50': !active
+    }"
+  >
+    <div class="flex h-[324px] bg-black">
+      <img class="-m-5" :src="imgSrc" :alt="title">
+      <div class="ml-10 py-6 pr-6 text-white">
+        <p class="text-lg font-bold flex">
+          <StarIcon class="text-gold w-4 h-4" />
+          {{ rating }}
+        </p>
+
+        <NuxtLink v-if="active" v-slot="{ href }" :to="`/movies/${movieId}`" custom>
+          <a :href="href">
+            <h1 class="text-[28px] font-medium">{{ title }}</h1>
+          </a>
+        </NuxtLink>
+
+        <h1 v-else class="text-[28px] font-medium">
+          {{ title }}
+        </h1>
+
+        <div class="flex items-center mt-3">
+          <h2 class="mr-2">
+            {{ year }}
+          </h2>
+          <span class="rounded-full w-2 h-2 bg-white/[0.5]" />
+          <h2 class="ml-2">
+            {{ genre }}
+          </h2>
+        </div>
+
+        <p class="line-clamp-8 mt-3 text-xs">
+          {{ overview }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,50 @@
 <script setup lang="ts">
 import type { Movies } from '~~/types/movies.type.js'
 import type { Genres } from '~~/types/genres.type.js'
-import type { Props as MovieItem } from '~~/components/Card.vue'
+import type { Props as MovieItem } from '~~/components/SliderItem.vue'
+
+type MovieItems = Omit<MovieItem, 'active'>[]
 
 interface TransformedMovies extends Omit<Movies, 'results'> {
-  results: MovieItem[];
+  results: MovieItems;
+}
+
+function normalizer (data: Movies) {
+  const results: MovieItems = data.results.map((movieItem) => {
+    const genre = genres.find((genreItem) => {
+      return genreItem.id === movieItem.genre_ids?.[0]
+    })
+
+    return {
+      movieId: movieItem.id,
+      title: movieItem.title,
+      year: movieItem.release_date?.substring(0, 4) || '',
+      genre: genre?.name || 'Unknown',
+      rating: parseInt(String(movieItem.vote_average * 10), 10) / 10,
+      image: movieItem.poster_path,
+      overview: movieItem.overview,
+    }
+  })
+  return { ...data, results }
 }
 
 const { data: responseGenres } = await useFetch('https://api.themoviedb.org/3/genre/movie/list?api_key=f0ed16cec6f5c089dab07bd0c89aa2f5&language=en-US')
 
 const genres = (responseGenres.value as Genres)?.genres || []
 
+const { data: responseSlider } = await useFetch(
+  'https://api.themoviedb.org/3/movie/now_playing?api_key=f0ed16cec6f5c089dab07bd0c89aa2f5&language=en-US&page=1',
+  {
+    transform: normalizer,
+  },
+)
+
+const sliderItems = (responseSlider.value as TransformedMovies)?.results.slice(0, 3) || []
+
 const { data: responseMovies } = await useFetch(
   'https://api.themoviedb.org/3/movie/popular?api_key=f0ed16cec6f5c089dab07bd0c89aa2f5&language=en-US&page=1',
   {
-    transform (data: Movies) {
-      const results: MovieItem[] = data.results.map((movieItem) => {
-        const genre = genres.find((genreItem) => {
-          return genreItem.id === movieItem.genre_ids?.[0]
-        })
-
-        return {
-          movieId: movieItem.id,
-          title: movieItem.title,
-          year: movieItem.release_date?.substring(0, 4) || '',
-          genre: genre?.name || 'Unknown',
-          rating: parseInt(String(movieItem.vote_average * 10), 10) / 10,
-          image: movieItem.poster_path,
-        }
-      })
-      return { ...data, results }
-    },
+    transform: normalizer,
   },
 )
 
@@ -39,8 +53,32 @@ const movies = (responseMovies.value as TransformedMovies)?.results || []
 </script>
 
 <template>
-  <section class="mt-16 h-[530px] text-white">
-    Hello world!
+  <section class="mt-32 h-[530px] text-white w-full overflow-hidden">
+    <div class="-mx-16 relative -left-16 grid grid-cols-3 gap-8">
+      <SliderItem
+        v-for="(item, index) in sliderItems"
+        :key="item.movieId"
+        :active="index === 1"
+        :movie-id="item.movieId"
+        :title="item.title"
+        :year="item.year"
+        :genre="item.genre"
+        :rating="item.rating"
+        :image="item.image"
+        :overview="item.overview"
+      />
+    </div>
+    <div class="w-full flex justify-center mt-12">
+      <div
+        v-for="(item, index) in sliderItems"
+        :key="item.movieId"
+        :class="{
+          'rounded-full h-4 mx-2': true,
+          'w-16 bg-decoration': index === 1,
+          'w-4 bg-white/[0.5]': index !== 1,
+        }"
+      />
+    </div>
   </section>
 
   <section class="bg-white/[0.05] h-[333px] text-neutral-200 pt-20 box-border">
